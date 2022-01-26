3416d49980a6473881e0dfe6a4f1357e855111ec
Ahn <abu89@naver.com>
2022-01-26 21:13:09 +0900

    [spring] request scope, provider
     + Use provider with request scope

--- core/src/main/java/hello/spring/beanscope/LogDemoService.java
+++ core/src/main/java/hello/spring/beanscope/LogDemoService.java
@@ -1,15 +1,17 @@
 package hello.spring.beanscope;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String testID) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service ID = " + testID);
     }
 }
